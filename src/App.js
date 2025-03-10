09fb958795e67e5118961215f1aa432d4288a4fa
Gracjan Gołębiewski <gra.golebiewski@gmail.com>
2025-03-10 22:41:22 +0100

    404 fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home/Home';
 import List from './components/List/List';
 import About from './components/About/About';
 import Favorite from './components/Favorite/Favorite';
+import NotFound from './components/NotFound/NotFound';
 
 const App = () => {
   return (
@@ -16,7 +17,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
-          <Route path="/list/:listId" element={<List />} /> 
+          <Route path="/list/:listId" element={<List />} />
+          <Route path="*" element={<NotFound />} /> 
         </Routes>
       </Container>
     </main>
